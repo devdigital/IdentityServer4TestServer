6379bf49e28e34999e3fb083f9cfa4ad579a4ab8
devdigital <devdigital.cloud@gmail.com>
2018-05-24 11:49:47 +0100

    chore: add client helper methods

--- Source/IdentityServer4TestServer/IdentityServerClient.cs
+++ Source/IdentityServer4TestServer/IdentityServerClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using IdentityModel.Client;
 
@@ -11,14 +12,38 @@ namespace IdentityServer4TestServer
 
         public IdentityServerClient(IIdentityServer server, string clientId, string clientSecret)
         {
-            if (string.IsNullOrWhiteSpace(clientId))
-            {
-                throw new ArgumentNullException(nameof(clientId));
-            }
-
             this.server = server ?? throw new ArgumentNullException(nameof(server));
             this.ClientId = clientId;
             this.ClientSecret = clientSecret;
+        }
+
+        public async Task<DiscoveryResponse> GetDiscovery()
+        {
+            using (var proxyHandler = this.server.CreateHandler())
+            {
+                using (var discoClient = new DiscoveryClient(this.server.BaseAddress.ToString(), proxyHandler))
+                {
+                    return await discoClient.GetAsync();
+                }
+            }
+        }
+
+        public async Task<TokenClient> CreateTokenClient()
+        {
+            var disco = await this.GetDiscovery();
+            return new TokenClient(disco.TokenEndpoint);
+        }
+
+        public async Task<TokenClient> CreateTokenClient(HttpMessageHandler innerHttpMessageHandler)
+        {
+            var disco = await this.GetDiscovery();
+            return new TokenClient(disco.TokenEndpoint, innerHttpMessageHandler);
+        }
+
+        public async Task<TokenClient> CreateTokenClient(HttpMessageHandler innerHttpMessageHandler, AuthenticationStyle authenticationStyle)
+        {
+            var disco = await this.GetDiscovery();
+            return new TokenClient(disco.TokenEndpoint, this.ClientId, this.ClientSecret, innerHttpMessageHandler, authenticationStyle);
         }
 
         public async Task<TokenResponse> GetToken()
@@ -36,6 +61,32 @@ namespace IdentityServer4TestServer
                     }                        
                 }                    
             }
+        }
+
+        public async Task<UserInfoResponse> GetUserInfo(string token, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var disco = await this.GetDiscovery();
+            using (var userInfoClient = new UserInfoClient(disco.UserInfoEndpoint))
+            {
+                return await userInfoClient.GetAsync(token, cancellationToken);
+            }
+        }
+
+        public async Task<IntrospectionClient> CreateIntrospectionClient(HttpMessageHandler innerHttpMessageHandler, BasicAuthenticationHeaderStyle headerStyle = BasicAuthenticationHeaderStyle.Rfc6749)
+        {
+            var disco = await this.GetDiscovery();
+            return new IntrospectionClient(
+                endpoint: disco.IntrospectionEndpoint,
+                clientId: this.ClientId,
+                clientSecret: this.ClientSecret,
+                innerHttpMessageHandler: innerHttpMessageHandler,
+                headerStyle: headerStyle);
+        }
+
+        public async Task<RequestUrl> CreateRequestUrl()
+        {
+            var disco = await this.GetDiscovery();
+            return new RequestUrl(disco.AuthorizeEndpoint);
         }
 
         public void Dispose()
